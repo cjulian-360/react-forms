0fdfa683bf62fd430e55cd146ba050486d95d628
cjulian <cjulian@360incentives.com>
2016-09-22 09:34:20 -0400

    separate test and coverage scripts

--- test/Calculator.test.js
+++ test/Calculator.test.js
@@ -1,11 +1,4 @@
 import { expect } from 'chai'
-describe('Array', ()=>{
-    describe('#indexOf()', ()=>{
-        it('should return -1 when the value is not present', function() {
-            expect([1,2,3].indexOf(4)).to.equal(-1);
-        });
-    });
-});
 
 import Calculator from '../source/Calculator'
 describe('Calculator', ()=>{
